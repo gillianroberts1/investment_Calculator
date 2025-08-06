2d9cd177a44556eb415b6da49f7043e45b278fcf
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2025-08-06 14:08:13 +0100

    header added

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,24 @@
+export default function UserInput() {
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label>Initial Investment</label>
+          <input type="number" required></input>
+        </p>
+        <p>
+          <label>Annual Investment</label>
+          <input type="number" required></input>
+        </p>
+        <p>
+          <label>Expected Return</label>
+          <input type="number" required></input>
+        </p>
+        <p>
+          <label>Duration</label>
+          <input type="number" required></input>
+        </p>
+      </div>
+    </section>
+  );
+}
