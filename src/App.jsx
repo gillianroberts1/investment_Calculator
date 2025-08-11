0b4fbfc796a7869152bf2c7fc24e3d751ba2e1a2
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2025-08-11 15:55:26 +0100

    state lifted up to App for calculating results

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
 import Header from "./Components/Header.jsx";
 import UserInput from "./Components/UserInput.jsx";
+import Results from "./Components/Results.jsx";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput userInput={userInput} onChange={handleChange}/>
+      <Results input={userInput}/>
     </>
   );
 }
